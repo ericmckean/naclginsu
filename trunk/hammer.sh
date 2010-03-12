85d4055b5f3854a7a2c345cb6c3684bc6df48977
alokp@chromium.org <alokp@chromium.org@e1669702-295f-11df-9f4f-a5c41f8f774a>
2010-03-12 22:44:55 +0000

    Added scons file. Review URL: http://codereview.appspot.com/460041

---
+++ trunk/hammer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Copyright (c) 2010 The Ginsu Project Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+CURR_DIR=`pwd -P`
+export SCONS_DIR=$CURR_DIR/third_party/scons/scons-local
+exec /bin/bash $CURR_DIR/third_party/swtoolkit/hammer.sh $*
