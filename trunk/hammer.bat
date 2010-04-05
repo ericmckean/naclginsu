2e9b034efe6760be886879e14d2a936bbbba2502
alokp@chromium.org <alokp@chromium.org@e1669702-295f-11df-9f4f-a5c41f8f774a>
2010-04-05 15:52:55 +0000

    Added support to build ginsu on windows. Review URL: http://codereview.appspot.com/820042

---
+++ trunk/hammer.bat
@@ -0,0 +1,8 @@
+@rem Copyright (c) 2010 The Ginsu Project Authors. All rights reserved.
+@rem Use of this source code is governed by a BSD-style license that can be
+@rem found in the LICENSE file.
+
+setlocal
+
+set SCONS_DIR=%~dp0third_party\scons\scons-local
+@call %~dp0third_party\swtoolkit\hammer.bat %*
