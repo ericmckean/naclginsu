8c1e14037bf5b82fe71f19e144b43fe9bebd5f9f
gwink@google.com <gwink@google.com@e1669702-295f-11df-9f4f-a5c41f8f774a>
2010-04-28 16:56:11 +0000

    Added code to build a cube component. Includes changes to existing file to support cgal's nef-polyhedra.
    Review URL: http://codereview.appspot.com/1003042

--- model/kernel.h
+++ model/kernel.h
@@ -5,13 +5,21 @@
 #ifndef GINSU_MODEL_KERNEL_H_
 #define GINSU_MODEL_KERNEL_H_
 
-#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 
 namespace ginsu {
 namespace model {
 
-typedef CGAL::Simple_cartesian<float> Kernel;
+// An exact kernel that supports cartesian coordinates. An exact kernel is
+// required for boolean computations. The cartesian coordinates make it easier
+// to interface with openSceneGraph.
+typedef CGAL::Exact_predicates_exact_constructions_kernel Kernel;
 
-}  // namespace math
+// Related types.
+typedef Kernel::Point_3 Point_3;
+typedef Kernel::Vector_3 Vector_3;
+typedef CGAL::Aff_transformation_3<Kernel> Aff_transformation_3;
+
 }  // namespace model
+}  // namespace ginsu
 #endif  // GINSU_MODEL_KERNEL_H_
