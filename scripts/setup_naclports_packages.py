6b0ca7a5ef126e75949973b62ec40f4673a2d26b
sanga@google.com <sanga@google.com@e1669702-295f-11df-9f4f-a5c41f8f774a>
2010-07-07 23:33:45 +0000

    Update naclports setup script to include gtest and gmock. Review URL: http://codereview.appspot.com/1680050

--- scripts/setup_naclports_packages.py
+++ scripts/setup_naclports_packages.py
@@ -20,21 +20,21 @@ def setup_packages():
   # get the packages from svn
   subprocess.call(["svn",
                    "checkout",
-                   "http://naclports.googlecode.com/svn/trunk@44",
+                   "http://naclports.googlecode.com/svn/trunk@49",
                    ".naclports"])
-  # cd in the packages directory.
-  path = os.path.join(".naclports", "src", "packages", "scripts")
+  root = os.getcwd()
+  path = os.path.join(root, ".naclports", "src", "packages", "scripts")
   os.chdir(path)
-  # cd into boost
-  os.chdir('boost_1_43_0')
-  # kick off script
-  subprocess.call(['./nacl-boost_1_43_0.sh'])
-  # cd into osg
-  os.chdir('../OpenSceneGraph-2.9.7')
-  # kick off script
-  subprocess.call(['./nacl-OpenSceneGraph-2.9.7.sh'])
+  packages = ["boost_1_43_0", "OpenSceneGraph-2.9.7", "gtest-1.5.0",
+              "gmock-1.5.0"]
+  # Go into each packag directory and run the script of each individual
+  # package.
+  for p in packages:
+    os.chdir(p)
+    subprocess.call(["./nacl-%s.sh" % p])
+    os.chdir(path)
   # cleanup
-  os.chdir('../../../../..')
+  os.chdir(root)
   shutil.rmtree('.naclports')
 
 if __name__ == '__main__':
