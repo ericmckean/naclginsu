64194337e7cac387fff296ae155e74469cb4b292
sanga@google.com <sanga@google.com@e1669702-295f-11df-9f4f-a5c41f8f774a>
2010-07-12 16:15:22 +0000

    Adding stubs for tests in c_salt. Review URL: http://codereview.appspot.com/1815041

---
+++ c_salt/string_type_test.cc
@@ -0,0 +1,23 @@
+// Copyright 2010 The Ginsu Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can
+// be found in the LICENSE file.
+
+#include "c_salt/string_type.h"
+
+#include <stdio.h>
+
+#include <iostream>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace {
+
+class StringTypeTest : public testing::Test {
+};
+
+TEST_F(StringTypeTest, CreateNPVariantCopy) {
+  // TODO(sanga):  Complete this test.
+}
+
+}  // namespace
