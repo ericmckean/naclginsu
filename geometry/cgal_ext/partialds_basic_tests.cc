9a3d50c93d360fdad39d7d2dac8546ae9ec56867
gwink@google.com <gwink@google.com@e1669702-295f-11df-9f4f-a5c41f8f774a>
2010-08-18 19:12:52 +0000

    Adding basic unit tests to PartialDS code, including scons files to build the partial DS and related unit tests.
    Review URL: http://codereview.appspot.com/1952043

---
+++ geometry/cgal_ext/partialds_basic_tests.cc
@@ -0,0 +1,52 @@
+// Copyright (c) 2010 The Ginsu Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <CGAL/Simple_cartesian.h>
+#include <gtest/gtest.h>
+#include "geometry/cgal_ext/partialds.h"
+
+namespace {
+
+using ginsu::geometry::PartialDS;
+
+class PartialDSTest : public ::testing::Test {
+ protected:
+  typedef CGAL::Simple_cartesian<double> Kernel;
+  typedef PartialDS<Kernel> PEMesh;
+
+  PartialDSTest() : mesh_(NULL) {}
+
+  virtual void SetUp() {
+    mesh_ = new PEMesh();
+  }
+
+  virtual void TearDown() {
+    delete mesh_;
+    mesh_ = NULL;
+  }
+
+ protected:
+  PEMesh* mesh_;
+};
+
+TEST_F(PartialDSTest, SimpleModelTest) {
+  ASSERT_TRUE(mesh_ != NULL);
+}
+
+TEST_F(PartialDSTest, TestMakeVertex) {
+  PartialDSTest::PEMesh::VertexHandle v;
+  v = mesh_->MakeVertex();
+  ASSERT_TRUE(v != NULL);
+  ASSERT_EQ(mesh_->vertices().size(), 1);
+  mesh_->DestroyVertex(v);
+  ASSERT_EQ(mesh_->vertices().size(), 0);
+}
+
+}  // namespace
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
