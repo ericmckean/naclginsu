9016570440a63330f22a4afb9f8b11c71852e728
dmichael@google.com <dmichael@google.com@e1669702-295f-11df-9f4f-a5c41f8f774a>
2010-09-01 17:30:58 +0000

    Fix the copyright in some recent files. Review URL: http://codereview.appspot.com/2127041

--- c_salt/callback_test.cc
+++ c_salt/callback_test.cc
@@ -1,5 +1,7 @@
-// Copyright 2010 Google Inc. All Rights Reserved.
-// Author: dmichael@google.com (Dave Michael)
+// Copyright 2010 The Ginsu Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can
+// be found in the LICENSE file.
+
 #include "c_salt/callback.h"
 
 #include <nacl/npruntime.h>
