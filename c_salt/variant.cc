2221ba5857707cd78d5f8da83eba66b7b346764d
dspringer@google.com <dspringer@google.com@e1669702-295f-11df-9f4f-a5c41f8f774a>
2010-09-09 19:16:04 +0000

    Beginning of c_salt::Variant class. Review URL: http://codereview.appspot.com/2125043

---
+++ c_salt/variant.cc
@@ -0,0 +1,16 @@
+// Copyright 2010 The Ginsu Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can
+// be found in the LICENSE file.
+
+#include "c_salt/variant.h"
+
+#include <cassert>
+#include "c_salt/scripting_bridge.h"
+
+namespace c_salt {
+
+// TODO(dspringer): implement this stuff.
+Variant::~Variant() {
+}
+
+}  // namespace c_salt
