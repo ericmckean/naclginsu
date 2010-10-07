b15e74b59fc30b960f2180506fe3e89b76dbc21f
dmichael@google.com <dmichael@google.com@e1669702-295f-11df-9f4f-a5c41f8f774a>
2010-10-07 22:06:32 +0000

    Use jsUnit to make testing (relatively) automated.  To run the test: 1.  Build with ./hammer.sh --mode=test. 2.  cd to scons-out/test/staging 3.  Run "python ./httpd.py 5103". 4.  Start a Chrome 6 browser with --enable-nacl. 5.  Point the browser at localhost:5103/c_salt_tests.html
    I also added some basic testing of properties (that currently fails because deletion doesn't work right for some reason).
    Review URL: http://codereview.appspot.com/2336045

--- trunk/c_salt/integration_tests/fake_instance.h
+++ trunk/c_salt/integration_tests/fake_instance.h
@@ -8,6 +8,7 @@
 #include "boost/shared_ptr.hpp"
 #include "c_salt/instance.h"
 #include "c_salt/integration_tests/method_tester.h"
+#include "c_salt/integration_tests/property_tester.h"
 
 class FakeInstance : public c_salt::Instance {
  public:
@@ -21,13 +22,13 @@ class FakeInstance : public c_salt::Instance {
  private:
   FakeInstance();  // Not implemented, do not use.
 
-  // A ScriptableNativeObject that provides methods to test.
-  // TODO(c_salt authors):  Create more Tester objects for other c_salt
-  // features.
+  // ScriptableNativeObjects for testing c_salt features.
   boost::shared_ptr<MethodTester> method_tester_;
+  boost::shared_ptr<PropertyTester> property_tester_;
 
-  // Get the method tester.  This is exposed via the ScriptingBridge.
+  // Getters for tester objects.  These are exposed via the ScriptingBridge.
   boost::shared_ptr<MethodTester> GetMethodTester();
+  boost::shared_ptr<PropertyTester> GetPropertyTester();
 };
 
 #endif  // C_SALT_INTEGRATION_TESTS_FAKE_INSTANCE_H_
