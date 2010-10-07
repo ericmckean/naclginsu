b3c842473f375ab7b878a4fe557e7b395622a198
dmichael@google.com <dmichael@google.com@e1669702-295f-11df-9f4f-a5c41f8f774a>
2010-10-07 22:06:32 +0000

    Use jsUnit to make testing (relatively) automated.  To run the test: 1.  Build with ./hammer.sh --mode=test. 2.  cd to scons-out/test/staging 3.  Run "python ./httpd.py 5103". 4.  Start a Chrome 6 browser with --enable-nacl. 5.  Point the browser at localhost:5103/c_salt_tests.html
    I also added some basic testing of properties (that currently fails because deletion doesn't work right for some reason).
    Review URL: http://codereview.appspot.com/2336045

--- c_salt/integration_tests/fake_instance.cc
+++ c_salt/integration_tests/fake_instance.cc
@@ -2,25 +2,26 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <cstdio>
-
 #include "c_salt/integration_tests/fake_instance.h"
 
 FakeInstance::FakeInstance(const NPP& npp_instance)
     : c_salt::Instance(npp_instance),
-      method_tester_(new MethodTester()) {
-  std::printf("FakeInstance constructor;  creating MethodScriptTest\n");
+      method_tester_(new MethodTester()),
+      property_tester_(new PropertyTester()) {
   this->CreateScriptingBridgeForObject(method_tester_);
+  this->CreateScriptingBridgeForObject(property_tester_);
 }
 
 FakeInstance::~FakeInstance() {
 }
 
 void FakeInstance::InitializeMethods(c_salt::ScriptingBridge* bridge) {
-  std::printf("Adding methods in FakeInstance\n");
   bridge->AddMethodNamed("getMethodTester",
                          this,
                          &FakeInstance::GetMethodTester);
+  bridge->AddMethodNamed("getPropertyTester",
+                         this,
+                         &FakeInstance::GetPropertyTester);
 }
 
 void FakeInstance::InitializeProperties(c_salt::ScriptingBridge* bridge) {
@@ -29,4 +30,8 @@ void FakeInstance::InitializeProperties(c_salt::ScriptingBridge* bridge) {
 
 boost::shared_ptr<MethodTester> FakeInstance::GetMethodTester() {
   return method_tester_;
+}
+
+boost::shared_ptr<PropertyTester> FakeInstance::GetPropertyTester() {
+  return property_tester_;
 }
